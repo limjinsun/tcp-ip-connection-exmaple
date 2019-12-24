217ceb0f0ad19737af0c5432e7e7a6c752b8938f
Jin Lim <limjinsun@gmail.com>
2019-12-24 18:04:53 +0000

    tcp-ip-server-client

---
+++ src/TCPClient2.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TCPClient2 {
+	
+	Socket socket = null;		//Server와 통신하기 위한 Socket
+	BufferedReader in = null;	//Server로부터 데이터를 읽어들이기 위한 입력스트림
+    BufferedReader in2 = null;	//키보드로부터 읽어들이기 위한 입력스트림
+    PrintWriter out = null;		//서버로 내보내기 위한 출력 스트림
+    InetAddress ia = null;
+	
+	public static void main(String[] args) {
+		
+		TCPClient2 client = new TCPClient2();
+		client.runClient();
+		
+	}
+	
+	public void runClient () {
+		
+        try {
+            ia = InetAddress.getByName("0.0.0.0");	//서버로 접속
+            socket = new Socket(ia,9000);
+            
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            in2 = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+            
+            System.out.println("클라이언트 소켓 : " + socket.toString());
+            
+            Thread sender = new Thread(new SendingThread());
+            sender.start();
+            
+            Thread reciever = new Thread(new ReceivingThread());
+            reciever.start(); 
+            
+        } catch(IOException e) {
+        	e.printStackTrace();
+        }
+        
+	}
+	
+	private class SendingThread implements Runnable {
+		
+		@Override
+		public void run() {
+			while(true) {
+				try {
+					System.out.print("서버로 보낼 메세지 : ");
+					String data = in2.readLine();		//키보드로부터 입력
+					out.println(data);					//서버로 데이터 전송
+					out.flush();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} 
+		}
+		
+	}
+	
+	private class ReceivingThread implements Runnable {
+		
+		@Override
+		public void run() {
+			while(true) {
+				try {
+	                String str2 = in.readLine();		//서버로부터 되돌아오는 데이터 읽어들임
+	                System.out.println("서버로부터 되돌아온 메세지 : " + str2);
+	            } catch(IOException e) {
+	            	e.printStackTrace();
+	            }
+			}
+		}
+		
+	}
+}
---
+++ src/TCPServer.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+	
+	ServerSocket serverSocket = null;
+	Socket socket = null;
+	
+	
+	public static void main(String[] args) {
+		
+		TCPServer tcpServer = new TCPServer();
+		tcpServer.runServer();
+		
+	}	
+	
+	public void runServer () {
+		
+		try {
+			serverSocket = new ServerSocket(9000);
+			System.out.println(serverSocket.getInetAddress().toString());
+			
+			while(true) {
+				
+				socket = serverSocket.accept(); // 기다리고 있다가, 클라이언트소켓이 접근하면, 업셋트 메소드를 실행해서 서버소켓을 리턴함. 
+				System.out.println(socket.toString());
+				
+				Thread serverSocketThread = new Thread(new ServerSocketThread(socket));
+				serverSocketThread.start();
+	
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private class ServerSocketThread implements Runnable {
+		
+		Socket socket;
+		BufferedReader in = null;
+		BufferedReader in2 = null;
+		PrintWriter out = null;
+		
+		public ServerSocketThread (Socket socket) {
+			this.socket = socket;
+		}
+		
+		@Override
+		public void run() {
+			try {
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // 인풋을 받는 버퍼리더 만듬. 
+				in2 = new BufferedReader(new InputStreamReader(System.in));
+				out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))); // 아웃풋을 내주는 라이터를 만듬.
+		            
+				Thread sender = new Thread(new SendingThread());
+		        sender.start();
+		            
+		        Thread reciever = new Thread(new ReceivingThread());
+		        reciever.start(); 
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+		}
+		
+		private class SendingThread implements Runnable {
+			
+			@Override
+			public void run() {
+				while(true) {
+					try {
+						System.out.print("클라이언트로 보낼 메세지 : ");
+						String data = in2.readLine();		
+						out.println(data);				
+						out.flush();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} 
+			}
+			
+		}
+		
+		private class ReceivingThread implements Runnable {
+			
+			@Override
+			public void run() {
+				while(true) {
+					try {
+		                String str2 = in.readLine();		//클라이언트로 부터 들어 오는메시지 읽기 
+		                if(str2 == null) {
+		                	socket.close();
+		                	return;
+		                } else {
+		                	System.out.println("클라이언트로 부터들어온 메세지 : " + str2);
+		                }
+		            } catch(IOException e) {
+		            	e.printStackTrace();
+		            }
+				}
+			}
+			
+		}	
+	}
+	
+}
